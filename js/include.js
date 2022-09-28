d8c04006e8ccd27e43dedc87aadfb9799686cc0e
MS-St-Andrae <113914146+MS-St-Andrae@users.noreply.github.com>
2022-09-28 17:17:29 +0200

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,26 @@
+document.write(`
+<div class="header">\
+      <div class="logo_space">\
+        <a href="index.html"><img alt="MS St. Andrä Logo" src="logo/ms_sta_logo.svg" id="logo"/></a>\
+      </div>\
+      <ul class="navbar">\
+        <li class="navbar_opt dropdown">\
+          Unsere Schule\
+          <div class="dropdown-content">\
+            <a href="ueber_uns.html" class="drop_opt">Die Schule</a>\
+            <a href="hausordnung.html" class="drop_opt">Hausordnung</a>\
+            <a href="team.html" class="drop_opt">Schulteam</a>\
+            <a href="schueler.html" class="drop_opt">SchülerInnen</a>\
+            <a href="projekte.html" class="navbar_opt">Projekte</a>\
+            <a href="galerie.html" class="drop_opt">Galerie</a>\
+          </div>\
+        </li>\
+        <a href="anmeldung.html"><li class="navbar_opt">Anmeldung</li></a>\
+        <a href="beratung.html"><li class="navbar_opt">Beratung</li></a>\
+        <a href="termine.html"><li class="navbar_opt">Termine</li></a>\
+        <a href="betreuung.html"><li class="navbar_opt">Tagesbetreuung</li></a>\
+        <a href="muttersprachen.html"><li class="navbar_opt">Muttersprachen</li></a>\
+      </ul>\
+    </div>\
+
+`);
